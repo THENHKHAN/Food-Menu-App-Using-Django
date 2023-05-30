6747345b72b9641c32d89860e0c8ba01b37ee44a
Noorul Huda Khan <noorulhuda101099@gmail.com>
2023-05-31 01:38:38 +0530

    Uses templates and loaded(using Load method of django) also getting data from DB to display on browser

--- foodProj/food/urls.py
+++ foodProj/food/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('',views.index , name="homePage" ), # home page - route - /food/
     path('about/',views.about , name="about" ), # must to use / after router name.
-    path('contact/',views.contact , name="contact" ), 
+    path('items/',views.listItems , name="items" ), 
+    path('passVar/',views.passVar , name="passVar" ), 
 ]
--- foodProj/food/views.py
+++ foodProj/food/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import items # to retrieve the data that are present in model
 # Create your views here.
 
 
 def index (request) :
-    return HttpResponse("<h1> hello Noor Home </h1>")
+    lst = items.objects.all()
+    template = loader.get_template('./index.html')
+    context = { # whatever data you have obtain from DB , you can send using context to browser/frontend.
+    'itemList' : lst,  # 'itemList' will work as a key which store all objects. means equavalent to lst above.
+       }
+
+    return HttpResponse(template.render(context,request)) # working but its better to use render 
+    # return render(request, './index.html',context)
+    # return HttpResponse("<h1> hello Noor Home </h1>")
 
 def about (request) :
     return HttpResponse("hello Noor it is about")
 
-def contact (request) :
-    return HttpResponse("hello Noor it is contact")
+def listItems (request) :
+    lst = items.objects.all() # it will list all the object or item stored in items class
+    print(lst)
+    return HttpResponse(lst)
+    
+    
+def passVar (request) : # thsi is how to pass variable
+    return render(request,"./index.html" , {'first_name': 'Noor', 'last_name': 'KHAN'})
