219572b14cfe9cf42389b3bc47b1cc395f0a417f
Noorul Huda Khan <noorulhuda101099@gmail.com>
2023-06-02 10:14:59 +0530

    able to send partucal id detail enter by user in URL box and made detail.html

--- foodProj/food/views.py
+++ foodProj/food/views.py
@@ -9,11 +9,12 @@ def index (request) :
     lst = items.objects.all()
     # template = loader.get_template('./index.html') No need if using render method
     context = { # whatever data you have obtain from DB , you can send using context to browser/frontend.
-    'itemList' : lst,  # 'itemList' will work as a key which store all objects. means equavalent to lst above.
+    'itemList' : lst,  # 'itemList' will work as a key which store all objects. means equivalent to lst above.
        }
 
     # return HttpResponse(template.render(context,request)) # working but its better to use render 
-    return render(request, './index.html',context)
+    return render(request, './index.html',context)#./index.htlm bcz django knows till templates directory and . means current directory. and ./ means in current directory a file name index.html
+
     # return HttpResponse("<h1> hello Noor Home </h1>")
 
 def about (request) :
@@ -31,5 +32,13 @@ def passVar (request) : # thsi is how to pass variable
 
 def detail(request,item_id) :
     #  when user click on any item it will direct to this detail URL/route.By id they will be differenctiated.
-    return HttpResponse(f"This is item no/id {item_id}")
+    # in this we need only that object whose id matched that's y we don't use   items.objects.all() beacuse we  need desire id.
+    item = items.objects.get(pk=item_id) # pk is primary key
+    print(f"item: {item}")
+    context ={
 
+    "item" : item, # this the above variable
+    }
+
+    # return HttpResponse(f"This is item no/id {item_id}")
+    return render(request, './detail.html' , context)
