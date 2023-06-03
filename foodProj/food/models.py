f21ed3a9f01ed0c71599a2c882bcf4884cf34939
Noorul Huda Khan <noorulhuda101099@gmail.com>
2023-06-03 12:14:12 +0530

    made card like img price , clickable detail btn using bootstrap

--- foodProj/food/models.py
+++ foodProj/food/models.py
@@ -9,4 +9,4 @@ class items (models.Model) :
     item_name= models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
-    
+    item_img = models.CharField(max_length=500 , default="https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg") #other option ImageField
